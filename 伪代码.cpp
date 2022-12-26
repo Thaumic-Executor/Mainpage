30c2ce424eea52e9f0b198087f0e3f72fd8157d2
Thaumic-Executor <110512274+Thaumic-Executor@users.noreply.github.com>
2022-12-26 20:28:53 +0800

    Create 伪代码.cpp

---
+++ 伪代码.cpp
@@ -0,0 +1,12 @@
+//让c++latex化，一些功能开发中
+#include<bits/stdc++.h>
+using namespace std;
+#define mod %
+#define eq ==
+#define neq !=
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define leq <=
+#define geq >=
+int main(){
+    return 0;
+}
